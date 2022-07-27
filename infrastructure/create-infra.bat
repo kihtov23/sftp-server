db543c047252175cde8741c637dfe374f1d006d7
Oleh Kikhtov <kihtov23@gmail.com>
2022-07-27 14:31:21 +0300

    Create user and update password

--- infrastructure/create-infra.bat
+++ infrastructure/create-infra.bat
@@ -10,3 +10,20 @@ az deployment group create ^
   --template-file "C:\Oleg\src\sftp-server\infrastructure\arm-template-storageaccount\template.json" ^
   --parameters "C:\Oleg\src\sftp-server\infrastructure\arm-template-storageaccount\parameters.json"
 pause
+
+
+az storage account local-user create ^
+  --account-name sftpstorageaccounteu ^
+  -g sftp-server ^
+  -n adminb ^
+  --permission-scope permissions=rw service=file resource-name=share2 ^
+  --ssh-authorized-key key="ssh-rsa a2V5" --has-ssh-key true --has-ssh-password --has-shared-key false
+
+az storage account local-user create ^
+  --account-name sftpstorageaccounteu ^
+  -g sftp-server ^
+  -n adminb ^
+  --permission-scope permissions=rw service=file resource-name=share2 ^
+  --has-ssh-password true
+
+az storage account local-user regenerate-password --account-name sftpstorageaccounteu -g sftp-server -n adminb 
---
+++ src/sftp-console-app/sftp-console-app/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var client = new HttpClient();
+
+
+// Call asynchronous network methods in a try/catch block to handle exceptions.
+try
+{
+    HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    // Above three lines can be replaced with new helper method below
+    // string responseBody = await client.GetStringAsync(uri);
+
+    Console.WriteLine(responseBody);
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("\nException Caught!");
+    Console.WriteLine("Message :{0} ", e.Message);
+}
+
+// create local user Azure API
+// https://docs.microsoft.com/en-us/rest/api/storagerp/local-users/create-or-update?tabs=HTTP#createlocaluser
+
+//var createLocalUser = new HttpRequestMessage(
+//            HttpMethod.Put,
+//            "https://management.azure.com/subscriptions/2b217d2b-cdc5-44ad-911f-0b89a2c2467c" +
+//            "/resourceGroups/sftp-server/providers/Microsoft.Storage" +
+//            "/storageAccounts/sftpstorageaccounteu/localUsers/testuser?api-version=2021-09-01")
+//{
+//    Headers =
+//            {
+//                { HeaderNames.Accept, "application/vnd.github.v3+json" },
+//                { HeaderNames.UserAgent, "HttpRequestsSample" }
+//            }
+//};
